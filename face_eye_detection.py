039cb94b7e56b6d98fec2629b9e6571f060b2edb
insung3511 <insung.park123@gmail.com>
2017-08-14 17:30:06 +0900

    Add files via upload

---
+++ face_eye_detection.py
@@ -0,0 +1,35 @@
+import numpy as np
+import cv2
+
+all_count = 0
+t_count = 0
+
+# multiple cascades: https://github.com/Itseez/opencv/tree/master/data/haarcascades
+
+#https://github.com/Itseez/opencv/blob/master/data/haarcascades/haarcascade_frontalface_default.xml
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+cap = cv2.VideoCapture(0)
+
+while 1:
+    ret, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    all_count = all_count + 1
+
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+	t_count = t_count + 1
+
+    cv2.imshow('img',img)
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
+print "All count :" , all_count
+print "Detection count :" , t_count
